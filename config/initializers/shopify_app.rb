ccaac6cbecd7d7a6081c53f6d172dcb597144288
Don Cooper <don_cooper@hotmail.com>
2017-07-17 15:20:22 -0400

    Third commit

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "4ceb689b522b40e9e0fb46776d88bc31"
   config.secret = "704e0912cca4db9769937536ab21c968"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_orders, read_products, read_customers"
   config.embedded_app = true
 end
