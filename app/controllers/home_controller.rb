bbfc62c9bbae4d8ca9a84a11cb461b42fb122b57
Don Cooper <don_cooper@hotmail.com>
2017-07-18 00:47:26 -0400

    commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all)
-    @product = ShopifyAPI::Product.find(:id)
     @orders = ShopifyAPI::Order.find(:all)
   end
 end
