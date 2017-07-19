f5ec70ccf2881974b0399cefdb464ec060948dbc
Don Cooper <don_cooper@hotmail.com>
2017-07-19 13:42:57 -0400

    Product images added to index page of app, basic styling changes.  Also starter code for manipulating assets, isolating whatever asset it is we want to copy/modify

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all)
     @orders = ShopifyAPI::Order.find(:all)
+    @assets = ShopifyAPI::Asset.find(:all)
   end
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_SECRET']
-  config.scope = "read_orders, read_products, read_customers"
+  config.scope = "read_orders, read_products, read_customers, read_themes, write_themes"
   config.embedded_app = true
 end
