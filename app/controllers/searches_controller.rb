b6ef643e20e153e1f2e765d64b4bb159768a735d
Ju-Hang Sin <juhangsin@gmail.com>
2017-07-21 17:05:17 -0400

    Search bar created, routed, working

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,8 @@
+class SearchesController < ShopifyApp::AuthenticatedController
+	def results
+		@products =
+			ShopifyAPI::Product.all.select do |product|
+				product.attributes["title"].downcase[params["q"].downcase]
+			end
+	end
+end
