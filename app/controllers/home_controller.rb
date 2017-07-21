413ab98ea6ecf176dffc177bce14fa23bac74474
Don Cooper <don_cooper@hotmail.com>
2017-07-21 16:22:32 -0400

    Javascript file switching document selectors based on what theme is used in the store, so only one snippet of code works across all free Shopify themes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all)
     @orders = ShopifyAPI::Order.find(:all)
+    @themes = ShopifyAPI::Theme.find(:all)
     @batches = Batch.all
   end
 end
