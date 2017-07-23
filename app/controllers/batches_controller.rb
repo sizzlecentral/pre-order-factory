a6629c333a8fd96df7695fc9cd05290e4107e344
Don Cooper <don_cooper@hotmail.com>
2017-07-23 16:14:10 -0400

    Setting up batch controller

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,9 +1,23 @@
-class BatchesController < ShopifyApp::AuthenticatedController
+class BatchesController < ApplicationController
 
-def index
-  # @productId = params[:id]
-  @product = ShopifyAPI::Product.find(params[:id])
-  @batches = Batch.where(product_id: params[:id])
-  @batch = Batch.new
-end
+  def index
+    @batches = Batch.all
+    @batch = Batch.new
+  end
+
+  def create
+    @batch = Batch.new(batch_params)
+    if @batch.save
+      redirect_to root
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def batch_params
+    params.require(:batch).permit(:product_id, :start_date, :end_date, :min_quantity, :num_sold, :active)
+  end
+
 end
