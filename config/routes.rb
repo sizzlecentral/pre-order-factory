60e733cc36dd60869cd01ea09572576b57108eb5
Don Cooper <don_cooper@hotmail.com>
2017-07-24 21:46:03 -0400

    Batch show page routed to home#batchshow.  @product and @orders is set up correctly, and batch view pages now show a list of orders corresponding to that batch.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
+  mount ShopifyApp::Engine, at: '/'
   root :to => 'home#index'
 
-  mount ShopifyApp::Engine, at: '/'
-
-  resources :batches
   resources :home
+  resources :batches, except: :show
+  get '/batches/:id' => 'home#batchshow'
   get '/search' => 'searches#results'
 
 end
