ec1ad94f3f7d65ec437371eda44d064fa9c69428
Don Cooper <don_cooper@hotmail.com>
2017-07-31 12:33:27 -0400

    Troubleshooting javascript not showing with heroku deployment, trying moving the js files to public folder in app

--- app/jobs/orders_create_job.rb
+++ app/jobs/orders_create_job.rb
@@ -1,22 +1,22 @@
 class OrdersCreateJob < ActiveJob::Base
-  # def perform(shop_domain:, webhook:)
-  #   shop = Shop.find_by(shopify_domain: shop_domain)
-  #
-  #   shop.with_shopify_session do
-  #     line_items = webhook[:line_items]
-  #     line_items.each do |item|
-  #       product_id = item[:product_id]
-  #       product_quantity = item[:quantity]
-  #       activeBatch = Batch.where(product_id: product_id, active: true)
-  #       activeBatch.each do |batch|
-  #         if batch.num_sold.nil?
-  #           batch.update!(num_sold: product_quantity)
-  #         else
-  #           batch.update!(num_sold: (batch.num_sold + product_quantity))
-  #         end
-  #       end
-  #     end
-  #   end
-  # end
+  def perform(shop_domain:, webhook:)
+    shop = Shop.find_by(shopify_domain: shop_domain)
+
+    shop.with_shopify_session do
+      line_items = webhook[:line_items]
+      line_items.each do |item|
+        product_id = item[:product_id]
+        product_quantity = item[:quantity]
+        activeBatch = Batch.where(product_id: product_id, active: true)
+        activeBatch.each do |batch|
+          if batch.num_sold.nil?
+            batch.update!(num_sold: product_quantity)
+          else
+            batch.update!(num_sold: (batch.num_sold + product_quantity))
+          end
+        end
+      end
+    end
+  end
 
 end
