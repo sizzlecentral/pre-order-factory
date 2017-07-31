fa1e7a9193ff1998d82958c1a8e04c856b64757f
Don Cooper <don_cooper@hotmail.com>
2017-07-31 13:07:52 -0400

    Adding config.serve_static_assets = true to application.rb to try and serve static assets

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module PreOrderFactory
     config.action_dispatch.default_headers.delete('X-Frame-Options')
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
